f4ef9258c07e3ec1c9d49be4be39b2bf16147926
Wouter van Nigtevegt <wvannigtevegt@gmail.com>
2016-01-19 15:31:43 +0100

    - Added missing ReservationResult.cs file

---
+++ S2M/Models/ReservationResult.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.ObjectModel;
+
+namespace S2M.Models {
+	public class ReservationResult : ApiResult {
+		public ObservableCollection<Reservation> Results { get; set; }
+	}
+}
